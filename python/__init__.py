d0be3e9e834311ac109e61d6748fc3ac4d8fe241
Joey Armstrong <jarmstrong@linuxfoundation.org>
2024-05-13 17:18:24 -0400

    [VOL-5291] - Add debugging for voltha-protos timeout
    [VOL-5344] - Add debugging for voltha-protos timeout
    
    makefiles/include.mk
    --------------------
      o pip install grpcio fails locally while trying to build 'wheel'.
      o Added a conditional to bypass virtualenv installation targets
        and variables to use the system installed interpreter and packages.
      o define $(activate-python) so we can override inlined calls
        "$(activate) && python".
    
    Makefile
    --------
      o Display timestamps for all targets built.
      o Updated every target to invoke $(call banner-{enter,leave}) .
      o Added targets *-enter and *-leave.  Used to display start/stop
        timestamps for makefile targets that lack shell commands.
      o Define macro TOX=tox so the command can be over-ridden.
    
    makefiles/etc/features.mk
    -------------------------
      o Display a timestamp when enter/leave banners are displayed.
      o Will be used to calculate elapsed time for individual targets.
    
    tox.ini
    -------
      o Add a pointer to tox.ini syntax help.
    
    Change-Id: Ic24e9aa30e170ad0105377d0d4bf6ac3850023da
    Signed-off-by: Joey Armstrong <jarmstrong@linuxfoundation.org>

--- python/__init__.py
+++ python/__init__.py
@@ -1,4 +1,5 @@
-# Copyright 2019-2024 Open Networking Foundation (ONF) and the ONF Contributors
+# -----------------------------------------------------------------------
+# Copyright 2019-2024 Open Networking Foundation Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,3 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# -----------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2019-2024 Open Networking Foundation Contributors
+# SPDX-License-Identifier: Apache-2.0
+# -----------------------------------------------------------------------
